3c521385b3b903714183d83a736319999412fe71
lucky <neupanesuman18@gmail.com>
2022-07-17 22:29:12 +0545

    string.dart is added

---
+++ string.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name1 = "my name is suman";
+  String name2 = "my birthplace is in pyuthan";
+  String para = """
+this video cover detail information about string in dart.
+you will see demo of string and how to use properties,method etc""";
+
+  print(name1);
+  print(name2);
+  print(para);
+}
