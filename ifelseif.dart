2deab32003ce5661b443d58f40e40f069b71f55c
lucky <neupanesuman18@gmail.com>
2022-07-18 22:57:41 +0545

    ifelseif is added

---
+++ ifelseif.dart
@@ -0,0 +1,14 @@
+void main() {
+  int noOFday = 6;
+
+  if (noOFday == 1) {
+    print("day is sunday");
+  } else if (noOFday == 5) {
+    print("today is thursday");
+  } else if (noOFday == 6) {
+    print("today is friday");
+  } else
+    () {
+      print("invalid day!!");
+    };
+}
