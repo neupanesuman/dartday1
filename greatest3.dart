5ba355c9d4e60fbe3193b8ca9b4bc8906f8acec6
lucky <neupanesuman18@gmail.com>
2022-07-18 23:16:43 +0545

    greatest among 3 number is added

---
+++ greatest3.dart
@@ -0,0 +1,13 @@
+void main() {
+  int num1 = 1000;
+  int num2 = 200;
+  int num3 = 300;
+
+  if (num1 > num2 && num1 > num3) {
+    print("num1 is greater i.e $num1");
+  } else if (num2 > num1 && num2 > num3) {
+    print("num2 is greatest i.e $num2");
+  } else if (num3 > num1 && num3 > num2) {
+    print("num3 is greatest i.e $num3");
+  }
+}
